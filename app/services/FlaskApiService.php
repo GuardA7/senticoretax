1505442f2d9b15ab60c9f859e14be314788a32a3
ANJAS Teguh <hyuzuru007@gmail.com>
2026-08-18 19:45:49 +0700

    api change

--- app/services/FlaskApiService.php
+++ app/services/FlaskApiService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 class FlaskApiService
 {
     protected $baseUrl =
-        'http://127.0.0.1:5000';
+        'https://web-production-eda61.up.railway.app';
 
     // =========================
     // NAIVE BAYES
